9bebe7f4025e2f14ce58b764f0beaae1450663a1
isaacjarquin <isaacjarquin@gmail.com>
2016-10-12 21:56:54 +0100

    adding webpack to the project

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,18 +1,6 @@
 /* global React ReactDOM */
-
 var div = React.DOM.div
-var h1 = React.DOM.h1
-
-var MyTitle = React.createClass({
-  render () {
-    return (
-      div(null,
-        h1(null, this.props.title)
-      )
-    )
-  }
-})
-
+var MyTitle = require('./MyTitle')
 var MyTitleFact = React.createFactory(MyTitle)
 
 var MyFirstComponent = (
---
+++ js/MyTitle.js
@@ -0,0 +1,14 @@
+var div = React.DOM.div
+var h1 = React.DOM.h1
+
+var MyTitle = React.createClass({
+  render () {
+    return (
+      div(null,
+        h1(null, this.props.title)
+      )
+    )
+  }
+})
+
+module.exports = MyTitle
