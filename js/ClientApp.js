e690277d902845596a77750ca46505e09b25166e
isaacjarquin <isaacjarquin@gmail.com>
2016-10-12 22:07:06 +0100

    setting up webpack

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,4 +1,6 @@
-/* global React ReactDOM */
+var React = require('react')
+var ReactDOM = require('react-dom')
+
 var div = React.DOM.div
 var MyTitle = require('./MyTitle')
 var MyTitleFact = React.createFactory(MyTitle)
