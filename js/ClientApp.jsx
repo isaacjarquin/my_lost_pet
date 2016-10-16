19c6e5604fa5f53835a344936fc169c374b085eb
isaacjarquin <isaacjarquin@gmail.com>
2016-10-16 21:31:36 +0100

    moving MissingPet into components folder and Search into pages folder

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./pages/Landing')
-const Search = require('./Search')
+const Search = require('./pages/Search')
 const { Router, Route, hashHistory } = require('react-router')
 
 const App = () => (
