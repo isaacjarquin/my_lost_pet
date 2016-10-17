208d798c79537953249973a25d15baefd34fc3f0
isaacjarquin <isaacjarquin@gmail.com>
2016-10-17 20:31:52 +0100

    Adding a layout to the app

--- js/pages/Landing.jsx
+++ js/pages/Landing.jsx
@@ -3,12 +3,10 @@ const MyTitle = require('../features/MyTitle')
 const { Link } = require('react-router')
 
 const Landing = () => (
-  <div className='app-container'>
-    <div className='home-info'>
-      <MyTitle title='My lost pet' color='rebeccapurple' />
-      <input className='search' type='text' placeholder='Search' />
-      <Link to='/search' className='browse-all'> or Brouse All</Link>
-    </div>
+  <div className='home-info'>
+    <MyTitle title='My lost pet' color='rebeccapurple' />
+    <input className='search' type='text' placeholder='Search' />
+    <Link to='/search' className='browse-all'> or Brouse All</Link>
   </div>
 )
 
