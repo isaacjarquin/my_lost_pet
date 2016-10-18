71ecd696d965a5ed5f3c8c15bc8a15b8d349ea9f
isaacjarquin <isaacjarquin@gmail.com>
2016-10-18 21:16:13 +0100

    adding state to the Search page

--- js/pages/Search.jsx
+++ js/pages/Search.jsx
@@ -2,12 +2,28 @@ const React = require('react')
 const data = require('../../public/mockData')
 const MissingPet = require('../features/MissingPet')
 
-const Search = () => (
-  <div className='container'>
-    {data.pets.map((pet) => (
-      <MissingPet {...pet} key={pet.id} />
-    ))}
-  </div>
-)
+const Search = React.createClass({
+  getInitialState () {
+    return {
+      searchTerm: 'search term'
+    }
+  },
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  },
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>My lost pet</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        {data.pets.map((pet) => (
+          <MissingPet {...pet} key={pet.id} />
+        ))}
+      </div>
+    )
+  }
+})
 
 module.exports = Search
