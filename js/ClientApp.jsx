ffd94b808aa65be7c27ae94dc175cf6cc63ebc24
isaacjarquin <isaacjarquin@gmail.com>
2016-10-22 20:42:10 +0100

    This commit add the folowing changes : * create pet details * add a link in the header component to the home directory * add header and background image to Layout

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,17 +1,37 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
+
 const Landing = require('./pages/Landing')
 const Search = require('./pages/Search')
+const PetDetails = require('./pages/PetDetails')
+
 const Layout = require('./layouts/Layout')
 const { Router, Route, IndexRoute, hashHistory } = require('react-router')
 
-const App = () => (
-  <Router history={hashHistory}>
-    <Route path='/' component={Layout}>
-      <IndexRoute component={Landing} />
-      <Route path='/search' component={Search} />
-    </Route>
-  </Router>
-)
+const { pets } = require('../public/mockData')
+
+const App = React.createClass({
+  assignPet (nextState, replace) {
+    const petArray = pets.filter((pet) => pet.id === nextState.params.id)
+
+    if (petArray.length < 1) {
+      return replace('/')
+    }
+
+    Object.assign(nextState.params, petArray[0])
+    return nextState
+  },
+  render () {
+    return (
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Landing} />
+          <Route path='/search' component={Search} pets={pets} />
+          <Route path='/details/:id' component={PetDetails} onEnter={this.assignPet} />
+        </Route>
+      </Router>
+    )
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- js/layouts/Layout.jsx
+++ js/layouts/Layout.jsx
@@ -1,9 +1,10 @@
-require('./layout.scss')
-
 const React = require('react')
+const Header = require('../features/Header')
 
 const Layout = (props) => (
   <div className='app-container'>
+    <Header title="My lost pet" />
+    <img className="layout-background" src="public/pets-background.jpg" />
     {props.children}
   </div>
 )
