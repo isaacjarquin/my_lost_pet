e0228327596c5741442663bbf41fd897842a86f0
isaacjarquin <isaacjarquin@gmail.com>
2016-10-24 22:59:07 +0100

    Add a link to each pet to the details page

--- js/features/missing_pet/MissingPet.jsx
+++ js/features/missing_pet/MissingPet.jsx
@@ -1,19 +1,22 @@
 const React = require('react')
 // require('missing_pet/MissingPet')
+const { Link } = require('react-router')
 
 import { Card, CardImg, CardText, CardBlock, CardTitle, CardSubtitle } from 'reactstrap'
 
 const MissingPet = (props) => (
-  <div className='pet'>
-    <Card>
-      <CardBlock className="pet-card">
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardTitle>{props.pet}, {props.breading}, {props.size}</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-        <CardText>{props.description}</CardText>
-      </CardBlock>
-    </Card>
-  </div>
+  <Link to={`/details/${props.id}`}>
+    <div className='pet'>
+      <Card>
+        <CardBlock className="pet-card">
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardTitle>{props.pet}, {props.breading}, {props.size}</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+          <CardText>{props.description}</CardText>
+        </CardBlock>
+      </Card>
+    </div>
+  </Link>
 )
 
 const { string } = React.PropTypes
@@ -22,7 +25,8 @@ MissingPet.propTypes = {
   breading: string.isRequired,
   size: string.isRequired,
   pet: string.isRequired,
-  description: string.isRequired
+  description: string.isRequired,
+  id: string.isRequired
 }
 
 module.exports = MissingPet
