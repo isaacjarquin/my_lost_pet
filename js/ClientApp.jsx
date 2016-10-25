2b2d0297cedfeda0328c3aec0ca0062b3865d751
isaacjarquin <isaacjarquin@gmail.com>
2016-10-25 23:26:29 +0100

    running the app server side through node

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,30 +1,34 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
-
 const Landing = require('./pages/Landing')
 const Search = require('./pages/Search')
 const PetDetails = require('./pages/PetDetails')
 
 const Layout = require('./layouts/Layout')
-const { Router, Route, IndexRoute, hashHistory } = require('react-router')
+const { Router, Route, IndexRoute, browserHistory } = require('react-router')
 
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
+
+const appRoutes = () => (
+  <Route path='/' component={Layout}>
+    <IndexRoute component={Landing} />
+    <Route path='/search' component={Search} />
+    <Route path='/details/:id' component={PetDetails} />
+  </Route>
+)
 
 const App = React.createClass({
   render () {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={PetDetails} />
-          </Route>
+        <Router history={browserHistory}>
+          {appRoutes()}
         </Router>
       </Provider>
     )
   }
 })
 
-ReactDOM.render(<App />, document.getElementById('app'))
+App.Routes = appRoutes
+
+module.exports = App
