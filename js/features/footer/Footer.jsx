be66b164c4312a51805ab0b3e00d98e48b9a87e6
isaacjarquin <isaacjarquin@gmail.com>
2016-11-06 18:19:22 +0000

    removing unecessary form from the footer

--- js/features/footer/Footer.jsx
+++ js/features/footer/Footer.jsx
@@ -9,10 +9,6 @@ const Footer = React.createClass({
     return (
       <footer className='container-fluid text-center'>
         <p>My lost pet Copyright</p>
-        <form className='form-inline'>Get deals:
-          <input type='email' className='form-control' size='50' placeholder='Email Address' />
-          <button type='button' className='btn btn-danger'>Sign Up</button>
-        </form>
       </footer>
     )
   }
