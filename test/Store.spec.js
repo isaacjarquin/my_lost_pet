3a501f4213677bf10f10a5e01f7c52acbe629960
isaacjarquin <isaacjarquin@gmail.com>
2016-11-19 17:04:25 +0000

    rename App.js to Store.js

--- test/Store.spec.js
+++ test/Store.spec.js
@@ -2,31 +2,11 @@
 
 const { expect } = require('chai')
 const React = require('react')
-const Search = require('../js/pages/search/index')
+const Navbar = require('../js/features/header/Navbar')
 const { shallow, mount } = require('enzyme')
 const { rootReducer } = require('../js/Store')
 const { pets } = require('../public/mockData')
-
-xdescribe('<Search />', () => {
-  const pets = {pets: []}
-
-  it('should render the brand', () => {
-    const wrapper = shallow(<Search pets={pets} />)
-
-    expect(wrapper.contains(<h1 className='brand'>My lost pet</h1>)).to.be.true
-    expect(wrapper.contains('My lost pet')).to.be.true
-  })
-
-  it('should filter correcly given a new state', () => {
-    const wrapper = mount(<Search />)
-    const input = wrapper.find('.search-input')
-
-    input.node.value = 'cat'
-    input.simulate('change')
-    expect(wrapper.state('searchTerm')).to.equal('cat')
-    expect(wrapper.find('.pet').length).to.equal(2)
-  })
-})
+const { Provider } = require('react-redux')
 
 describe('Store', () => {
   const initialState = {
