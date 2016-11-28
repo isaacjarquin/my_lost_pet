a1bac9b16d865bfef0a1c1e07252f6036aa4e43a
isaacjarquin <isaacjarquin@gmail.com>
2016-11-28 21:49:19 +0000

    remove lint errors and fix broken test

--- js/pages/landing/index.jsx
+++ js/pages/landing/index.jsx
@@ -41,20 +41,25 @@ class Landing extends React.Component {
             </div>
 
             <h1 className='w3-jumbo w3-text-white w3-hide-small'><b>My lost pet</b></h1>
+
             <div className='lost'>
               <h1 className='w3-text-white'>Encuentralo con nosotros</h1>
-              <p><input className='w3-input w3-border' type='text' placeholder='Encontrado en' /></p>
-              <Link to='/search'><h6><button className='w3-btn w3-white w3-padding-large w3-large w3-opacity w3-hover-opacity-off'>Buscar</button></h6></Link>
-            </div>
-
-            <div className='looking-for-home'>
-              <h1 className='w3-text-white'>Adopta una mascota</h1>
               <form onSubmit={this.gotoSearch}>
-                <p><input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='w3-input w3-border' type='text' placeholder='Buscando en' /></p>
+                <p><input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='w3-input w3-border' type='text' placeholder='Encontrado en' /></p>
                 <Dropdown dropDownTypes={petTypes} dropDownTitle={'Pet type '} />
               </form>
               <Link to='/search'><h6><button className='w3-btn w3-white w3-padding-large w3-large w3-opacity w3-hover-opacity-off'>Buscar</button></h6></Link>
             </div>
+
+            <div className='looking-for-home'>
+              <h1 className='w3-text-white'>Dale un hogar</h1>
+              <form onSubmit={this.gotoSearch}>
+                <p><input value={this.props.searchTerm} onChange={this.handleSearchTermEvent} className='w3-input w3-border' type='text' placeholder='Actualmente en' /></p>
+                <Dropdown dropDownTypes={petTypes} dropDownTitle={'Pet type '} />
+              </form>
+              <Link to='/search'><h6><button className='w3-btn w3-white w3-padding-large w3-large w3-opacity w3-hover-opacity-off'>Buscar</button></h6></Link>
+            </div>
+
           </div>
         </header>
 
