da19cbdde7bdee50d85865582b226c28fbfb7320
isaacjarquin <isaacjarquin@gmail.com>
2016-11-29 21:25:25 +0000

    Add owner form details.
    In order tu put in contact the pets owner and the person who found
    the pet, we need the owner personal details.
    this commit add a collapsable form in the pets details section the
    pets owner can send his details to contact the other person.

--- js/pages/pet-details/index.jsx
+++ js/pages/pet-details/index.jsx
@@ -11,7 +11,6 @@ class PetDetails extends React.Component {
     return (
       <div className='container'>
         <div className='pet-info'>
-
           <div className='pet-card w3-container w3-white w3-margin w3-padding-large'>
             <div className='pet-card_title w3-center '>
               <h3>{pet}, {breading}, {size}</h3>
@@ -21,8 +20,28 @@ class PetDetails extends React.Component {
             <div className='pet-card_body w3-justify'>
               <img src={image} alt='Men in Hats' className='w3-padding-12' />
               <p>{description}</p>
-              <p className='w3-right'><button className='w3-btn w3-white w3-border'><b>Responder</b></button></p>
+              <p className='w3-right'>
+                <button data-toggle='collapse' data-target='#response-form' className='w3-btn w3-white w3-border'>
+                  <b>Responder</b>
+                </button>
+              </p>
               <p className='w3-clear' />
+            </div>
+            <div id='response-form' className='collapse'>
+              <div className='w3-white w3-margin'>
+                <div className='w3-container w3-padding w3-black'>
+                  <h4>Introduce tus datos de contacto</h4>
+                </div>
+                <div className='w3-container w3-white'>
+                  <p className='form-introduction'>Introduce tus datos para poder ponerte en contacto con la persona que esta a cargo de tu mascota.</p>
+                  <form>
+                    <p><input className='w3-input w3-border' type='text' placeholder='Nombre' /></p>
+                    <p><input className='w3-input w3-border' type='text' placeholder='e-mail' /></p>
+                    <p><input className='w3-input w3-border' type='text' placeholder='Numero de telefono' /></p>
+                    <p><button type='button' className='w3-btn-block w3-padding w3-green w3-opacity w3-hover-opacity-off'>Enviar mis datos</button></p>
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </div>
