37291f6210ad438b4ae253ffe44e6566f46d3f6a
isaac jarquin <isaacjarquin@gmail.com>
2017-07-23 13:33:10 +0100

    wip

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -72,6 +72,21 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    }),
     new ExtractTextPlugin('[name].css'),
     new webpack.DefinePlugin({
       'process.env': {
