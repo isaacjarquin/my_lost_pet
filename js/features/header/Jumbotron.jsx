07b9ea74fbe107744e75ba86832383ba8d2e4635
isaac jarquin <isaacjarquin@gmail.com>
2018-05-16 22:58:32 +0100

    add map to search result page

--- js/features/header/Jumbotron.jsx
+++ js/features/header/Jumbotron.jsx
@@ -1,9 +1,22 @@
 const React = require('react')
+import GoogleMapReact from 'google-map-react';
 
 const Jumbotron = React.createClass({
   render () {
+    const map = {
+      lat: 40.4167754,
+      lng: -3.7037901999999576,
+      zoom: 5
+    }
     return (
       <div className='jumbotron'>
+        <GoogleMapReact
+          defaultCenter={{ lat: map.lat, lng: map.lng }}
+          defaultZoom={map.zoom}
+          zoom={map.zoom}
+          center={{ lat: map.lat, lng: map.lng }}
+        >
+        </GoogleMapReact>
         <div className='container text-center brand-name'>
           <h1>My Lost Pet</h1>
         </div>
