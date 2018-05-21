70a6489399fd4978ed4e1bbadcb7f3eb733adaee
isaac jarquin <isaacjarquin@gmail.com>
2018-05-21 21:58:41 +0100

    render empty map when no results

--- js/features/header/Jumbotron.jsx
+++ js/features/header/Jumbotron.jsx
@@ -17,26 +17,55 @@ class Jumbotron extends React.Component {
     }
   }
 
+  renderEmptyMap() {
+    const { pets } = this.props
+
+    return (
+      <GoogleMapReact
+        zoom={2}
+        center={{ lat: 40.4167754, lng: -3.7037901999999576 }}
+      >
+        {pets.map((pet) => {
+          return (
+            <Marker
+              lat={pet.latitud}
+              lng={pet.longitud}
+              icon={<FaMapMarker size={25} color={"orange"} />}
+            />
+          )
+        })}
+      </GoogleMapReact>
+    )
+  }
+
+  renderMapWithResults() {
+    const { pets } = this.props
+
+    return (
+      <GoogleMapReact
+        zoom={15}
+        center={{ lat: pets[0].latitud, lng: pets[0].longitud }}
+      >
+        {pets.map((pet) => {
+          return (
+            <Marker
+              lat={pet.latitud}
+              lng={pet.longitud}
+              icon={<FaMapMarker size={25} color={"orange"} />}
+            />
+          )
+        })}
+      </GoogleMapReact>
+    )
+  }
+
   render() {
     const { map } = this.state
     const { pets } = this.props
 
     return (
       <div className='jumbotron'>
-        <GoogleMapReact
-          zoom={map.zoom}
-          center={{ lat: map.lat, lng: map.lng }}
-        >
-          {pets.map((pet) => {
-            return (
-              <Marker
-                lat={pet.latitud}
-                lng={pet.longitud}
-                icon={<FaMapMarker size={25} color={"orange"} />}
-              />
-            )
-          })}
-        </GoogleMapReact>
+        { pets.length > 0 ? this.renderMapWithResults() : this.renderEmptyMap() }
       </div>
     )
   }
